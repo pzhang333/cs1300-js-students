cbad01cf7ae7ac040ede93920333f2e946d24c3e
Patrick Zhang <patrick_zhang1@brown.edu>
2020-10-29 20:24:10 -0400

    Finished lab

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
 // TODO: REPLACE YOUR TOKEN
-var apiToken = "?token=YOUR_TOKEN_HERE";
+var apiToken = "?token=aQaQFiA2lqAA8ugLxl54JZwg8Pe2Z5rZeSYQzOz6yQU";
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -21,11 +21,30 @@ const corsPromise = () =>
   });
 
 // THIS IS SOME SAMPLE CODE FOR HOW TO USE PROMISES -- feel free to adapt this into a function!
-corsPromise().then(
-  (request) =>
-    (request.onload = request.onerror = function () {
-      // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
-    })
-);
 
 //// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
+function onClick() {
+  corsPromise().then(
+    (request) =>
+      (request.onload = request.onerror = function () {
+        const allData = JSON.parse(request.response).data;
+        const even = allData.filter(data => data.common_name.length % 2 == 0);
+        const container = document.getElementById("content");
+        for (const data of even) {
+          const img = document.createElement("img");
+          const text = document.createTextNode(data.common_name);
+          const spacer1 = document.createElement("br");
+          const spacer2 = document.createElement("br");
+          const spacer3 = document.createElement("br");
+
+          img.setAttribute("src", data.image_url);
+
+          container.appendChild(img);
+          container.appendChild(spacer1);
+          container.appendChild(text)
+          container.appendChild(spacer2);
+          container.appendChild(spacer3);
+        }
+      })
+  );
+}
